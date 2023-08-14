d7676beda5fb46fc4f452f06807c02a7ed31510e
gfmcarey <gfmcarey@gmail.com>
2023-08-14 16:19:21 -0400

    removed hello world

---
+++ stacks/StorageStack.js
@@ -0,0 +1,15 @@
+import { Table } from "sst/constructs";
+
+export function StorageStack({ stack, app }) {
+    // Create the DynamoDB table
+    const table = new Table(stack, "Notes", {
+        fields: {
+            userId: "string",
+            noteId: "string",
+        },
+        primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+    });
+    return {
+        table,
+    };
+}
